8a5f206cc7b64e4ff98d8016959681ce94ad625c
BugPone (Leo Le) <34802737+BugPone@users.noreply.github.com>
2018-11-25 00:50:45 -0800

    Add files via upload
    testing something

---
+++ Working_But_Without_MVC/View.java
@@ -0,0 +1,46 @@
+package a7_Send;
+
+import java.awt.Container;
+import java.util.Queue;
+
+/**
+ * 
+ * @author Team 5
+ *
+ */
+/**
+ * View class contains the GUI and input for the game. 
+ * Also generates actions from the user that then passes it to the Controller class.
+ * @author Team 5
+ *
+ */
+public class View {
+	Queue<Object> messages;
+	Container GUI;
+	
+	public View() {
+		
+	}
+	
+	/**
+	 * Changes the GUI based on the actions performed within the game
+	 */
+	public void change() {
+		
+	}
+	
+	/**
+	 * Adds actions to the queue based on key events. 
+	 */
+	public void addToQueue() {
+		
+	}
+	
+	/**
+	 * Detects if a collision occurred between two objects. Sends info to controller.
+	 */
+	
+	public void detectCollision() {
+		
+	}
+}
